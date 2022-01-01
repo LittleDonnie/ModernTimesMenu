65d9566f6d8428deb94e0dbaaaaaf4f647b0daa1
Monty Montgomery <poweredbyplants@gmail.com>
2022-01-01 08:57:21 -0600

    Initial Commit

---
+++ Module 4 Challenge 9 Pickers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Module 4 Challenge 9 Pickers
+//
+//  Created by Monty Montgomery on 1/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Module 4 Challenge 9 Pickers/Module_4_Challenge_9_PickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module_4_Challenge_9_PickersApp.swift
+//  Module 4 Challenge 9 Pickers
+//
+//  Created by Monty Montgomery on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Module_4_Challenge_9_PickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
